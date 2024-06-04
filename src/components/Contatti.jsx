d745936e99a63b6e782e1d9f228b3c5bc050d210
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-06-04 15:17:09 +0200

    contact form okey

--- src/components/Contatti.jsx
+++ src/components/Contatti.jsx
@@ -1,18 +1,58 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Button, Form } from "react-bootstrap";
+import { sendCustomEmail } from "./Email";
+import { useState } from "react";
 
 const Contatti = () => {
+  const [details, setDetails] = useState({
+    subject: "",
+    message: "",
+    to_email: "alessialuongoworkspace@gmail.com",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendCustomEmail(details);
+  };
+
   return (
     <Container id="contatti">
       <Row className="p-5 justify-content-center align-content-center">
         <Col xs={12} md={8} lg={8}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-            odit eos accusamus, quia tenetur autem dolores harum facilis, alias
-            pariatur neque. Consequuntur architecto, quisquam unde debitis
-            cumque aperiam laborum? Tenetur.
-          </p>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="subject">
+              <Form.Label>La sua email</Form.Label>
+              <Form.Control
+                type="email"
+                required
+                placeholder="email@example.com"
+                value={details.subject}
+                onChange={(e) =>
+                  setDetails({ ...details, subject: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="message">
+              <Form.Label>Mi scriva qui</Form.Label>
+              <Form.Control
+                as="textarea"
+                required
+                rows={3}
+                value={details.message}
+                onChange={(e) =>
+                  setDetails({ ...details, message: e.target.value })
+                }
+              />
+            </Form.Group>
+            <Button
+              type="submit"
+              disabled={!details.subject || !details.message}
+            >
+              Invia
+            </Button>
+          </Form>
         </Col>
       </Row>
     </Container>
---
+++ src/components/Email.jsx
@@ -0,0 +1,23 @@
+import emailjs from "@emailjs/browser";
+
+const sendCustomEmail = (details) => {
+  emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
+  emailjs
+    .send(
+      import.meta.env.VITE_EMAIL_SERVICE_ID,
+      import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+      {
+        to_email: details.to_email,
+        subject: details.subject,
+        message: details.message,
+      }
+    )
+    .then((response) => {
+      console.log("Email sent successfully:", response);
+    })
+    .catch((error) => {
+      console.error("Error sending email:", error);
+    });
+};
+
+export { sendCustomEmail };
